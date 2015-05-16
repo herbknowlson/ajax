daa570a7c7a470966a903a924ff0e46a6d0ff598
Herb Knowlson <herbknowlson.gmail.com>
2015-05-15 22:08:16 -0500

    install and test rspec and rake

--- testRakeV01.rb
+++ testRakeV01.rb
@@ -7,62 +7,76 @@
 #----------------------------------------------------
 #
 
-
-MAX_FILES = 100
-results = "indexNames.json"
-results2 = "indexNames.txt"
-#puts results
-
-myfolder = "C:/xampp/htdocs/Ajax/drop images here/Index/"
-
-keys = []
-values = []
-i = 0
-x = 0
-Dir.entries(myfolder).each do |fullName|
-  if x>1
-    keys[i] = fullName[0..2]
-    values[i] = fullName[4..35]
-    i = i+1
+describe "rpec example" do
+  it 'step-01 should test rake' do
+    puts "testRake"
   end
-  x = x +1   
- end
-
-tempHash = Hash[keys.zip values]
-require 'json'
-tempHash.to_json
-puts JSON.pretty_generate(tempHash)
-
+end 
+#SERVER_URL
+#it 'step-01 should test rake' do
+#  response = RestClient.get("#{SERVER_URL}/patientlist", :x_auth_token => @xAuthToken, :params => {:listtype => 'Census', :moduleId => PM_MODULE_ID, :siteId => SITEID}){|response, request, result|
+#    Common.debug(request, response, DEBUG)
+#    response = JSON.parse(response)
+#    response["responseCode"].should eql 0
+#    response["model"]["patients"].count.should > 1
+#  }
+#end
 #
-# Write the file names to a file and print the results
+#MAX_FILES = 100
+#results = "indexNames.json"
+#results2 = "indexNames.txt"
+##puts results
 #
-File.open(results,"w") do |f|
-  f.write(tempHash.to_json)
-end
-
-tdCount = 0
-trCount = 0
-File.open(results2,"w") do |f|
-  tr = "<table style=width:75% CLASS=boldtable><tr>"
-  f.puts tr
-  for i in 0 ... keys.size
-    tdCount = tdCount + 1
-    td = "<td><a href='index.php?select=true&index1=" + keys[i] + "*'>" + values[i] + "</a>"
-    td2 = " -  <a href='index.php?select=true&index1=" + keys[i] + "*&index2=gifs'>gifs</a>  </td>"
-    f.puts td + td2
-    if tdCount > 2
-      trCount = trCount + 1
-      tr = "</tr><tr>"
-      f.puts tr
-      tdCount = 0
-    end
-    if trCount > 2
-      tr = "</tr></table><br><table style=width:75% CLASS=boldtable><tr>"
-      f.puts tr
-      trCount = 0
-    end
-  end #outer loop
-end # close the file
+#myfolder = "C:/xampp/htdocs/Ajax/drop images here/Index/"
+#
+#keys = []
+#values = []
+#i = 0
+#x = 0
+#Dir.entries(myfolder).each do |fullName|
+#  if x>1
+#    keys[i] = fullName[0..2]
+#    values[i] = fullName[4..35]
+#    i = i+1
+#  end
+#  x = x +1   
+# end
+#
+#tempHash = Hash[keys.zip values]
+#require 'json'
+#tempHash.to_json
+#puts JSON.pretty_generate(tempHash)
+#
+##
+## Write the file names to a file and print the results
+##
+#File.open(results,"w") do |f|
+#  f.write(tempHash.to_json)
+#end
+#
+#tdCount = 0
+#trCount = 0
+#File.open(results2,"w") do |f|
+#  tr = "<table style=width:75% CLASS=boldtable><tr>"
+#  f.puts tr
+#  for i in 0 ... keys.size
+#    tdCount = tdCount + 1
+#    td = "<td><a href='index.php?select=true&index1=" + keys[i] + "*'>" + values[i] + "</a>"
+#    td2 = " -  <a href='index.php?select=true&index1=" + keys[i] + "*&index2=gifs'>gifs</a>  </td>"
+#    f.puts td + td2
+#    if tdCount > 2
+#      trCount = trCount + 1
+#      tr = "</tr><tr>"
+#      f.puts tr
+#      tdCount = 0
+#    end
+#    if trCount > 2
+#      tr = "</tr></table><br><table style=width:75% CLASS=boldtable><tr>"
+#      f.puts tr
+#      trCount = 0
+#    end
+#  end #outer loop
+#end # close the file
 
 #<td><a href='index.php?select=true&index1=0_F*'>Favorites</a></td>
 #open(results2,'w') { |f| newArray.each {|x| f << "'<img src = " + '"' + x.to_s + '"' + ">'" + "\n" }}
